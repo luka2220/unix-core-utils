5f7791ea14c0f54fb3b8dc677daa4479005a1a0e
Luka Piplica <piplicaluka64@gmail.com>
2025-11-15 16:02:49 -0500

    Flag type and operation's parsing

---
+++ README.md
@@ -0,0 +1,9 @@
+# Unix core utils
+
+`wc, ls, cp, rm, mv, cat, head, tail, and sort`
+
+## WC
+
+- `wc ...path` -> word count output
+  <br>i.e `25     60      500 src/main.rs` (num lines, num words, num bytes, filename)
+- `wc -l ...path` -> outputs line count of a file
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,57 @@
-use std::{env, process};
+use clap::{ArgAction, Parser};
+use std::{path, process};
 
-fn main() {
-    let proc: Vec<String> = env::args().collect();
-    validate_args(&proc);
+/// wc cli command
+#[derive(Parser, Debug)]
+struct Args {
+    /// file path
+    #[arg(value_name = "FILE")]
+    file_paths: Vec<path::PathBuf>,
 
-    println!("{:?}", proc)
+    /// print the bytes count
+    #[arg(short = 'c', long = "bytes", action = ArgAction::SetTrue, conflicts_with = "chars")]
+    bytes: bool,
+
+    /// print the chars count 
+    #[arg(short = 'm', long = "chars", action = ArgAction::SetTrue, conflicts_with = "bytes")]
+    chars: bool,
 }
 
-fn validate_args(args: &Vec<String>) {
-    if args.len() < 2 {
-        println!("No arguments provided");
+/// Flag types from the command option
+enum FlagKind {
+    Bytes,
+    Chars,
+    Default
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.file_paths.len() < 1 {
+        println!("No file provided");
         process::exit(0)
+    }
+
+    let count_mode: FlagKind;
+
+    if args.bytes {
+        count_mode = FlagKind::Bytes;
+    } else if args.chars {
+        count_mode = FlagKind::Chars;
+    } else {
+        count_mode = FlagKind::Default
+    }
+
+    match count_mode {
+        FlagKind::Bytes => process_bytes(&args.file_paths),
+        FlagKind::Chars => println!("process wc chars"),
+        FlagKind::Default => println!("no flags provided"),
     }
 }
 
-// fn display(args: &Vec<String>) {
-//     let command = args.get(1);
-// }
+/// Gets the byte count of all passed in files
+fn process_bytes(files: &Vec<path::PathBuf>) {
+    for item in files.iter() {
+        println!("{:?}", item.to_string_lossy())
+    }
+}
